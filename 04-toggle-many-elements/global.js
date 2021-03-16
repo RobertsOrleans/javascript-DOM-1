73963875deccb6d92872645066f2ac0a1994714e
RobertsOrleans <robertsorleans@gmail.com>
2021-03-16 15:43:59 +0200

    DOM Atomic 04

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,15 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function(){
+    const toggle = document.getElementsByClassName('toggle_me');
+    const button = document.getElementById("button");
+
+    button.addEventListener('click', function(){
+        Array.from(toggle).forEach((e) => {
+            if(e.style.display == 'none') {
+                e.style.display = 'block'
+            }else{
+                e.style.display = 'none'
+            }
+        });
+    });
+});
