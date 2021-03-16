14de7d0f1886b7d27f96258a68ec9481f2aa815c
RobertsOrleans <robertsorleans@gmail.com>
2021-03-16 15:58:15 +0200

    DOM Atomic 06

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,9 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function(){
+    const textField = document.getElementById("full_name");
+    textField.addEventListener("keyup", function() {
+
+    const greeting = document.getElementById("greeting");
+    greeting.innerHTML = ("Hello there, " + textField.value + "!");
+  });
+});
