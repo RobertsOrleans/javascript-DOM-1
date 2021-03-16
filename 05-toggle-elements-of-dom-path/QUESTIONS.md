49a72636a6b58337f1a23b7c5034a6c92df72434
RobertsOrleans <robertsorleans@gmail.com>
2021-03-16 15:53:00 +0200

    DOM Atomic 05

--- 05-toggle-elements-of-dom-path/QUESTIONS.md
+++ 05-toggle-elements-of-dom-path/QUESTIONS.md
@@ -6,4 +6,4 @@
 
 > Describe the contract you used for finding the movies to toggle in the DOM. How is this function different from other functions that find elements in the DOM?
 
-Your reply here...
+I used .querySelectorAll to get all of the second movie elements because they are all a part of a list, with a matching class. This is different by selecting every child of the element than specific elements with the class.
--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,15 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function(){
+    const toggle = document.querySelectorAll('.second_five li')
+    const button = document.getElementById("toggle_button");
+
+    button.addEventListener('click', function(){
+        toggle.forEach((e) => {
+            if(e.style.display == 'block') {
+                e.style.display = 'none';
+            } else {
+                e.style.display = 'block';
+            }
+        });
+    });
+});
